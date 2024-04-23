0415c66776cefc957eb7fd1d24ff1903b43e0e95
jesse <jesse@localhost>
2024-04-23 12:29:00 +0200

    add some unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub enum Value {
     Bool(bool),
 }
 
+#[derive(Eq, Hash, PartialEq)]
 pub struct Identifier {
     pub provider: String,
     pub name: String,
@@ -19,10 +20,79 @@ pub struct Entry {
 
 pub struct Table {
     pub identifier: Identifier,
-    pub entries: HashMap<String, Entry>,
+    pub entries: HashMap<Identifier, Entry>,
 }
 
 pub struct Database {
     pub name: String,
     pub tables: HashMap<Identifier, Table>,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use crate::*;
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn sample_db() -> () {
+        let mut db = Database {
+            name: "sample".to_string(),
+            tables: HashMap::new(),
+        };
+        db.tables.insert(
+            Identifier {
+                provider: "sample".to_string(),
+                name: "planets".to_string(),
+            },
+            Table {
+                identifier: Identifier {
+                    provider: "sample".to_string(),
+                    name: "planets".to_string(),
+                },
+                entries: HashMap::new(),
+            }
+        );
+    }
+
+    #[test]
+    fn sample_db_with_items() -> () {
+        let mut db = Database {
+            name: "sample".to_string(),
+            tables: HashMap::new(),
+        };
+        db.tables.insert(
+            Identifier {
+                provider: "sample".to_string(),
+                name: "planets".to_string(),
+            },
+            Table {
+                identifier: Identifier {
+                    provider: "sample".to_string(),
+                    name: "planets".to_string(),
+                },
+                entries: HashMap::new(),
+            }
+        );
+        db.tables.get_mut(&Identifier {
+            provider: "sample".to_string(),
+            name: "planets".to_string(),
+        }).unwrap().entries.insert(
+            Identifier {
+                provider: "sample".to_string(),
+                name: "earth".to_string(),
+            },
+            Entry {
+                identifier: Identifier {
+                    provider: "sample".to_string(),
+                    name: "earth".to_string(),
+                },
+                value: Value::Str("blue".to_string()),
+            }
+        );
+    }
 }
